0616a654c6deee287625755a8f809267c056f3c5
jaysson18 <50361707+jaysson18@users.noreply.github.com>
2025-03-20 17:44:56 +0200

    Add initial theme files including header, footer, sidebar, and styles

---
+++ wp-content/themes/example-theme/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
+</head>
+
+<body>
+<div class="container">
+    <header class="page-header">
+        <div class="header-top-left">
+            <?php the_custom_logo() ?>
+        </div>
+        <div class="header-top-right">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">About us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
